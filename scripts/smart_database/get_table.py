f7928fac411922b7b2a03dc8bb916f3343d2a981
Sam McSweeney <sammy.mcsweeney@gmail.com>
2023-01-02 11:31:36 +0800

    Added param option to get_table.py, and removed the now obsolete get_algorithm_settings.py

--- scripts/smart_database/get_table.py
+++ scripts/smart_database/get_table.py
@@ -8,14 +8,14 @@ import json
 import requests
 import smart_database_auth as smart
 
-def get_table(table, token=None, base_url=None):
+def get_table(table, token=None, base_url=None, **kwargs):
 
     # Open a session to the SMART database
     smart_session = smart.Session(token=token, base_url=base_url)
 
     # Send a GET request to the appropriate view
     view = f'api/{table}/'
-    rows = smart_session.get(view)
+    rows = smart_session.get(view, **kwargs)
 
     return rows
 
@@ -30,14 +30,19 @@ def main():
     parser.add_argument('table', help='The table to be polled.')
     parser.add_argument('--format', default='csv', help="Output format: json, csv (default)")
     parser.add_argument('--delimiter', default=',', help="When --format=csv, use this delimiter to separate columns")
+    parser.add_argument('--param', action='append', help="Supply an extra parameter to the GET request (can be used multiple times)")
 
     args = parser.parse_args()
+
+    # Convert the params argument into a dictionary
+    params_dict = {key_value[0]: key_value[1] for key_value in [param.split("=") for param in args.param]} if args.param else None
 
     try:
         rows = get_table(
             args.table,
             token=args.token,
             base_url=args.base_url,
+            params=params_dict,
         )
         rows.raise_for_status()
     except requests.exceptions.HTTPError as err:
